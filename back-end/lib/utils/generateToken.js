d88f71d97e89a7acf2c3e46351048a755a9490d9
TheHellSide <ninja.fab.03@gmail.com>
2025-09-01 21:56:14 +0200

    3:00:35

---
+++ back-end/lib/utils/generateToken.js
@@ -0,0 +1,17 @@
+import jwt from 'jsonwebtoken';
+
+export const generateTokenAndSetCookie = (uid, res) => {
+    const token = jwt.sign({uid}, process.env.JWT_SECRET, 
+        {
+            expiresIn: '15d'
+        }
+    );
+
+    res.cookie("jwt", token, {
+        maxAge: 15*24*60*60*1000, // ms
+        httpOnly: true, // noXSS
+        sameSite: "strict", // noCSRF
+        secure: process.env.NODE_ENV !== 'development',
+    });
+};
+
---
+++ back-end/models/user.model.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    full_name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    followers: [
+        {
+            type: mongoose.Schema.Types.ObjectId, // refers to 'user model'.
+            ref: "User",
+            default: []
+        }
+    ],
+    following: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            default: []
+        }
+    ],
+    profile_image: {
+        type: String,
+        default: ""
+    },
+    cover_image: {
+        type: String,
+        default: ""
+    },
+    bio: {
+        type: String,
+        default: ""
+    },
+    link: {
+        type: String,
+        default: ""
+    },
+},
+    {
+        timestamps: true // created_at
+    }
+);
+
+// 'users'
+const User = mongoose.Model("User", userSchema);
